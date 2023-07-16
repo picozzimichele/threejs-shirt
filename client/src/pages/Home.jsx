8e7ddc84d29f6c7a2284bfdf08f58aa9e85cb644
Michele Picozzi <michele.picozzi@hotmail.it>
2023-07-16 16:29:55 +0800

    created valtio store and pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from "../config/motion";
+import state from "../store";
+
+export default function Home() {
+    return <div>Home</div>;
+}
